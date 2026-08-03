564f4a5ddf28c285ca0c3864a6b473a865c5dfa8
richyfabz <dammifabz@gmail.com>
2026-08-03 22:02:42 +0100

    fix image asset paths

--- src/data/assets.ts
+++ src/data/assets.ts
@@ -1,11 +1,13 @@
+import { siteImages } from './siteImages';
+
 export const familyAssets = {
-  lounge: new URL('../../img/customer1.webp', import.meta.url).href,
-  selfie: new URL('../../img/download (1).webp', import.meta.url).href,
-  villa: new URL('../../img/download (2).webp', import.meta.url).href,
-  premiumVilla: new URL('../../img/download (3).webp', import.meta.url).href,
-  skyline: new URL('../../img/download (4).webp', import.meta.url).href,
-  duplex: new URL('../../img/Duplex house.jpg', import.meta.url).href,
-  luxuryVilla: new URL('../../img/download (5).webp', import.meta.url).href,
-  storyBackground: new URL('../../img/Chinna Wallpaper.webp', import.meta.url).href,
-  map: new URL('../../img/map.jpg', import.meta.url).href,
+  lounge: siteImages.courtyardHome,
+  selfie: siteImages.havanaVilla,
+  villa: siteImages.modernEstate,
+  premiumVilla: siteImages.poolVilla,
+  skyline: siteImages.lagosSkyline,
+  duplex: siteImages.heroDuplex,
+  luxuryVilla: siteImages.glassMansion,
+  storyBackground: siteImages.modernLuxury,
+  map: siteImages.enuguCity,
 };
